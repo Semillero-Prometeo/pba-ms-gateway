628e9236b580c9a44d31ea530e628814114d2dec
Santiago Arias <santiari05@hotmail.com>
2026-03-26 21:31:01 -0500

    Rename fields in SetRoleDto for consistency: changed 'userId' to 'user_id' and 'rolesId' to 'roles_id'.

--- src/auth-ms/roles/dto/set-role.dto.ts
+++ src/auth-ms/roles/dto/set-role.dto.ts
@@ -3,10 +3,10 @@ import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from 'class-validator';
 export class SetRoleDto {
   @IsString({ message: 'El campo userId debe ser de tipo string' })
   @IsNotEmpty({ message: 'El campo userId no debe estar vacío' })
-  userId: string;
+  user_id: string;
 
   @IsArray({ message: 'El campo rolesId debe ser un array' })
   @ArrayMinSize(1, { message: 'Debe incluir al menos un roleId' })
   @IsString({ each: true, message: 'Cada roleId debe ser de tipo string' })
-  rolesId: string[];
+  roles_id: string[];
 }
