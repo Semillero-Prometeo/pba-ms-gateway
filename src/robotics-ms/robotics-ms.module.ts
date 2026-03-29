8fd19ffa587bfdcf76fd0e8a0d8008da9fd397c6
Santiago Arias <santiari05@hotmail.com>
2026-03-29 15:31:51 -0500

    Import ActionModule into RoboticsMsModule to enhance functionality and modularity.

--- src/robotics-ms/robotics-ms.module.ts
+++ src/robotics-ms/robotics-ms.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NatsModule } from 'src/transports/nats.module';
 import { RoboticsMsController } from './robotics-ms.controller';
+import { ActionModule } from './action/action.module';
 
 @Module({
-  imports: [NatsModule],
+  imports: [NatsModule, ActionModule],
   controllers: [RoboticsMsController],
 })
 export class RoboticsMsModule {}
